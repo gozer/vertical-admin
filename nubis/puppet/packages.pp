01a5b9d25e198b553b07d982e02034b78a08c8f8
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-03-08 13:11:24 -0500

    Vertica admin host

---
+++ nubis/puppet/packages.pp
@@ -0,0 +1,30 @@
+$vsql_major_version = '8.1'
+$vsql_version = "${vsql_major_version}.1-13"
+
+package { 'vsql':
+  ensure          => present,
+  provider        => 'rpm',
+  name            => 'vertica-client-fips',
+  source          => "https://my.vertica.com/client_drivers/${vsql_major_version}.x/${vsql_version}/vertica-client-fips-${vsql_version}.${::architecture}.rpm",
+  install_options => [
+    '--noscripts',
+  ],
+}
+
+# Fix missing error XML file
+file { '/opt/vertica/lib64/en-US':
+  ensure  => 'link',
+  target  => '../en-US',
+  require => [
+    Package['vsql'],
+  ],
+}
+
+# Put VSQL on the path
+file { '/usr/local/bin/vsql':
+  ensure  => 'link',
+  target  => '/opt/vertica/bin/vsql',
+  require => [
+    Package['vsql'],
+  ],
+}
