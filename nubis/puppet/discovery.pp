4b847cd6af8b232da297d91c52e463fa4aa6b18e
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-03-09 12:58:51 -0500

    expose Vertica metrics to Prometheus

--- nubis/puppet/discovery.pp
+++ nubis/puppet/discovery.pp
@@ -1,6 +1,13 @@
 include nubis_discovery
 nubis::discovery::service { $project_name:
-  tags     => [ '%%PROJECT%%', '%%PURPOSE%%' ],
+  tags     => [ '%%PURPOSE%%' ],
   tcp      => 22,
+  interval => '30s',
+}
+
+nubis::discovery::service { 'vertica-metrics':
+  tags     => [ 'metrics' ],
+  http     => 'http://localhost:9113',
+  port     => 9113,
   interval => '30s',
 }
