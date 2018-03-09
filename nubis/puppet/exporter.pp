3f089831aa226c9fa6b83cdfb22bd94c0adfcca4
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-03-09 11:31:30 -0500

    enable DBMS exporter

--- nubis/puppet/exporter.pp
+++ nubis/puppet/exporter.pp
@@ -12,3 +12,36 @@ staging::file { '/usr/local/bin/dbms_exporter':
   command => 'chmod 755 /usr/local/bin/dbms_exporter',
   path    => ['/sbin','/bin','/usr/sbin','/usr/bin','/usr/local/sbin','/usr/local/bin'],
 }
+
+file {'/etc/dbms_exporter':
+  ensure => directory,
+}
+
+file {'/etc/dbms_exporter/query.yaml':
+  ensure => file,
+  owner  => root,
+  group  => root,
+  mode   => '0644',
+  source => 'puppet:///nubis/files/query.yaml',
+}
+
+systemd::unit_file { 'dbms_exporter.service':
+  content => @("EOT")
+[Unit]
+Description=Prometheus DBMS Exporter
+Wants=basic.target
+After=basic.target network.target
+
+[Service]
+Restart=on-failure
+RestartSec=10s
+
+Environment=DATA_SOURCE_NAME=DSN=metrics
+
+ExecStart=/usr/local/bin/dbms_exporter -driver odbc -queryfile /etc/dbms_exporter/query.yaml
+
+EOT
+} ~> service { 'dbms_exporter':
+  ensure => 'stopped',
+  enable => false,
+}
