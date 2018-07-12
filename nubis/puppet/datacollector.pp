4d7946dbf5ab7f866d99a532fb3c23065d37185b
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-07-12 14:08:55 -0400

    systemd for datacollector

--- nubis/puppet/datacollector.pp
+++ nubis/puppet/datacollector.pp
@@ -15,3 +15,22 @@ file { '/opt/mi/datacollector':
     File['/opt/mi'],
   ],
 }
+
+systemd::unit_file { 'datacollector.service':
+  content => @("EOT")
+[Unit]
+Description=Metrics Insights DataCollector
+Wants=basic.target
+After=basic.target network.target
+
+[Service]
+Restart=on-failure
+RestartSec=10s
+
+ExecStart=/bin/bash -c '. /etc/profile.d/proxy.sh && /opt/mi/datacollector/bin/insightd -f /etc/datacollector/config.ini -l DEBUG start'
+
+EOT
+} ~> service { 'datacollector':
+  ensure => 'stopped',
+  enable => false,
+}
